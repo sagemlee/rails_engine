7b88d52134961549c611f6ec6e070fa417dde427
Sage Lee <sagemcklee@gmail.com>
2020-07-15 17:23:06 -0600

    Rearrange seed file to properly import cvs data

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,44 @@ end
 
 puts "There are now #{Customer.count} rows in the customers table"
 
+csv_text_merchant = File.read(Rails.root.join('app','data', 'merchants.csv'))
+csv_merchant = CSV.parse(csv_text_merchant, :headers => true)
+csv_merchant.each do |row|
+  t = Merchant.new
+  t.name = row['name']
+  t.created_at = row['created_at']
+  t.updated_at = row['updated_at']
+  t.save
+end
+puts "There are now #{Merchant.count} rows in the merchants table"
+
+csv_text_items = File.read(Rails.root.join('app','data', 'items.csv'))
+csv_items = CSV.parse(csv_text_items, :headers => true)
+csv_items.each do |row|
+  t = Item.new
+  t.name = row['name']
+  t.merchant_id = row['merchant_id']
+  t.description = row['description']
+  t.unit_price= (row['unit_price'].to_f/100)
+  t.created_at = row['created_at']
+  t.updated_at = row['updated_at']
+  t.save
+end
+puts "There are now #{Item.count} rows in the Items table"
+
+csv_text_invoice = File.read(Rails.root.join('app','data', 'invoices.csv'))
+csv_invoice = CSV.parse(csv_text_invoice, :headers => true)
+csv_invoice.each do |row|
+  t = Invoice.new
+  t.customer_id = row['customer_id']
+  t.merchant_id = row['merchant_id']
+  t.status = row['status']
+  t.created_at = row['created_at']
+  t.updated_at = row['updated_at']
+  t.save
+end
+puts "There are now #{Invoice.count} rows in the Invoices table"
+
 csv_text_invoice_items= File.read(Rails.root.join('app','data', 'invoice_items.csv'))
 csv_invoice_items = CSV.parse(csv_text_invoice_items, :headers => true)
 csv_invoice_items.each do |row|
@@ -37,43 +75,6 @@ end
 
 puts "There are now #{InvoiceItem.count} rows in the InvoiceItems table"
 
-csv_text_invoice = File.read(Rails.root.join('app','data', 'invoices.csv'))
-csv_invoice = CSV.parse(csv_text_invoice, :headers => true)
-csv_invoice.each do |row|
-  t = Invoice.new
-  t.customer_id = row['customer_id']
-  t.merchant_id = row['merchant_id']
-  t.status = row['status']
-  t.created_at = row['created_at']
-  t.updated_at = row['updated_at']
-  t.save
-end
-puts "There are now #{Invoice.count} rows in the Invoices table"
-
-csv_text_items = File.read(Rails.root.join('app','data', 'items.csv'))
-csv_items = CSV.parse(csv_text_items, :headers => true)
-csv_items.each do |row|
-  t = Item.new
-  t.name = row['name']
-  t.merchant_id = row['merchant_id']
-  t.description = row['description']
-  t.unit_price= (row['unit_price'].to_f/100)
-  t.created_at = row['created_at']
-  t.updated_at = row['updated_at']
-  t.save
-end
-puts "There are now #{Item.count} rows in the Items table"
-
-csv_text_merchant = File.read(Rails.root.join('app','data', 'merchants.csv'))
-csv_merchant = CSV.parse(csv_text_merchant, :headers => true)
-csv_merchant.each do |row|
-  t = Merchant.new
-  t.name = row['name']
-  t.created_at = row['created_at']
-  t.updated_at = row['updated_at']
-  t.save
-end
-puts "There are now #{Merchant.count} rows in the merchants table"
 
 csv_text_transactions = File.read(Rails.root.join('app','data', 'transactions.csv'))
 csv_transactions = CSV.parse(csv_text_transactions, :headers => true)
