62f588209e51e5a8ed61ee7cf1db5319bab674d6
Sage Lee <sagemcklee@gmail.com>
2020-07-15 17:34:19 -0600

    Update models with through relationships

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,6 @@
 class Invoice < ApplicationRecord
     has_many :invoice_items, dependent: :destroy
+    has_many :items, through: :invoice_items
     belongs_to :customer
     belongs_to :merchant
 end
